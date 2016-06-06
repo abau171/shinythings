3da49c3fb0e4dc4f81aca270969f2c7f1a3cd97a
Andrew Bauer <abau171@gmail.com>
2016-06-06 16:52:11 -0400

    added surface colors

--- _shinythings/render.c
+++ _shinythings/render.c
@@ -14,43 +14,46 @@ static vector_t camera_look = {0.0, 0.0, -3.0};
 static vector_t camera_right = {1.92, 0.0, 0.0};
 static vector_t camera_down = {0.0, -1.08, 0.0};
 
-static sphere_t sphere_a = {{0.0, 0.0, -1.0}, 0.5};
-static plane_t plane_a = {{0.0, -0.5, 0.0}, {0.0, 1.0, 0.0}};
+static sphere_model_t sphere_a = {{{0.0, 0.0, -1.0}, 0.5}, {{0.0, 0.4, 1.0}}};
+static plane_model_t plane_a = {{{0.0, -0.5, 0.0}, {0.0, 1.0, 0.0}}, {{1.0, 0.4, 0.0}}};
 static light_t light_a = {{1.0, 1.0, 1.0}};
 
-static bool trace_ray_object(vector_t ray_start, vector_t ray_direction, vector_t* hit, vector_t* normal)
+static bool trace_ray_object(vector_t ray_start, vector_t ray_direction, vector_t* hit, vector_t* normal, surface_t** surface)
 {
     float direction_magnitude = vector_magnitude(ray_direction);
     ray_start = vector_add(ray_start, vector_scale(ray_direction, 0.01 / direction_magnitude));
 
     vector_t sphere_hit;
-    bool sphere_success = sphere_intersect(sphere_a, camera, ray_direction, &sphere_hit);
+    bool sphere_success = sphere_intersect(sphere_a.sphere, camera, ray_direction, &sphere_hit);
     if (sphere_success) {
         *hit = sphere_hit;
-        *normal = vector_normalize(vector_sub(sphere_hit, sphere_a.center));
+        *normal = vector_normalize(vector_sub(sphere_hit, sphere_a.sphere.center));
+        *surface = &sphere_a.surface;
         return true;
     }
 
     vector_t plane_hit;
-    bool plane_success = plane_intersect(plane_a, camera, ray_direction, &plane_hit);
+    bool plane_success = plane_intersect(plane_a.plane, camera, ray_direction, &plane_hit);
     if (plane_success) {
         *hit = plane_hit;
-        *normal = plane_a.normal;
+        *normal = plane_a.plane.normal;
+        *surface = &plane_a.surface;
         return true;
     }
 
     return false;
 }
 
-static color_t get_diffuse_color(vector_t hit, vector_t normal)
+static color_t get_diffuse_color(vector_t hit, vector_t normal, surface_t* surface)
 {
     vector_t to_light = vector_sub(light_a.position, hit);
     vector_t obj_hit, obj_normal;
-    if (!trace_ray_object(hit, to_light, &obj_hit, &obj_normal)) {
+    surface_t* obj_surface;
+    if (!trace_ray_object(hit, to_light, &obj_hit, &obj_normal, &obj_surface)) {
         float distance_2 = vector_magnitude_2(to_light);
         float diffuse_scalar = fmaxf(0.0, vector_dot(to_light, normal) / sqrtf(distance_2));
         float intensity = 1.0 / distance_2;
-        return (color_t) {diffuse_scalar * intensity, diffuse_scalar * intensity, diffuse_scalar * intensity};
+        return (color_t) {diffuse_scalar * intensity * surface->color.r, diffuse_scalar * intensity * surface->color.g, diffuse_scalar * intensity * surface->color.b};
     }
     return (color_t) {0.0, 0.0, 0.0}; // AMBIENT
 }
@@ -58,9 +61,10 @@ static color_t get_diffuse_color(vector_t hit, vector_t normal)
 static color_t trace_ray(vector_t ray_start, vector_t ray_direction)
 {
     vector_t obj_hit, obj_normal;
-    bool obj_success = trace_ray_object(ray_start, ray_direction, &obj_hit, &obj_normal);
+    surface_t* obj_surface;
+    bool obj_success = trace_ray_object(ray_start, ray_direction, &obj_hit, &obj_normal, &obj_surface);
     if (obj_success) {
-        return get_diffuse_color(obj_hit, obj_normal);
+        return get_diffuse_color(obj_hit, obj_normal, obj_surface);
     }
     return (color_t) {0.0, 0.0, 0.0}; // BACKGROUND
 }
--- _shinythings/scene.h
+++ _shinythings/scene.h
@@ -1,6 +1,22 @@
 #ifndef SCENE_H
 #define SCENE_H
 
+#include "geometry.h"
+
+typedef struct {
+    color_t color;
+} surface_t;
+
+typedef struct {
+    sphere_t sphere;
+    surface_t surface;
+} sphere_model_t;
+
+typedef struct {
+    plane_t plane;
+    surface_t surface;
+} plane_model_t;
+
 typedef struct {
     vector_t position;
 } light_t;
