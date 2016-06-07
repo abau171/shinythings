1336bd60d2e6b5f2e4256367e36bc3e835317bb0
Andrew Bauer <abau171@gmail.com>
2016-06-07 14:09:04 -0400

    added .obj model loading

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 import shinythings
+import shinythings.obj
 
 scene = shinythings.Scene()
 scene.add_sphere(((0.25, 0.35, 0.25), 0.15), ((1, 0, 0.4), 0.05, 10))
@@ -8,7 +9,9 @@ scene.add_light((3, 3, 3), (10, 10, 10))
 scene.add_model(
     [(0, 0.65, 0), (-0.5, 0.15, -0.25), (0.5, 0.15, -0.25), (0, 0.15, 0.25)],
     [(0, 2, 1), (0, 1, 3), (0, 3, 2), (1, 2, 3)],
-    ((0, 0.4, 1.0), 0, 1))
+    ((0, 0.4, 1), 0, 1))
+#scene.add_model(*shinythings.obj.parse_obj("objects/box.obj"))
+#scene.add_model(*shinythings.obj.parse_obj("objects/suzanne.obj"))
 image = scene.render()
 image.show()
 
---
+++ shinythings/obj.py
@@ -0,0 +1,39 @@
+def fix_index(index):
+    return index if index < 1 else index - 1
+
+def parse_face_parameter(param):
+    vertex_index = None
+    texture_coord_index = None
+    normal_index = None
+    subparams = [sp for sp in param.split("/")]
+    if len(subparams) == 0:
+        raise Exception
+    vertex_index = fix_index(int(subparams[0]))
+    if len(subparams) >= 2 and subparams[1] != "":
+        texture_coord_index = fix_index(int(subparams[1]))
+    if len(subparams) >= 3 and subparams[2] != "":
+        normal_index = fix_index(int(subparams[2]))
+    return vertex_index, texture_coord_index, normal_index
+
+def parse_obj(filename):
+    vertices = []
+    triangles = []
+    with open(filename) as obj_file:
+        for line in obj_file:
+            tokens = line.split()
+            if len(tokens) == 0:
+                continue
+            if tokens[0] == "v":
+                vertex = tuple(0.1 * float(dim_str) for dim_str in tokens[1:4])
+                vertices.append(vertex)
+            elif tokens[0] == "f":
+                vertex_indices = []
+                for token in tokens[1:]:
+                    vertex_index, texture_coord_index, normal_index = parse_face_parameter(token)
+                    vertex_indices.append(vertex_index)
+                if len(vertex_indices) >= 3:
+                    triangles.append((vertex_indices[0], vertex_indices[1], vertex_indices[2]))
+                if len(vertex_indices) >= 4:
+                    triangles.append((vertex_indices[2], vertex_indices[3], vertex_indices[0]))
+    return (vertices, triangles, ((0, 0.4, 1), 0.2, 10))
+
